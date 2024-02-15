3a44e942071c55eb389cbae2ed784de2447fe524
Carlo Hildebrandt <carlo.hildebrandt@nordcloud.com>
2024-02-15 22:49:19 +0100

    add typing animation and poem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Title from './Title';
 
 function App() {
   return (
     <>
-      <h1>@here</h1>
+      <div style={{ width: "90vw", maxWidth: "1296px", margin: "auto", marginTop: "5vh" }}>
+        <Title />
+        <p style={{ color: "#D1D2D3", fontSize: "5vh" }}>
+          In the realm of code, a developer delves deep, <br />
+          Crafting digital worlds, where dreams do leap. <br />
+          Focused and absorbed, in the task at hand,<br />
+          Till a ping disrupts, like a shifting sand.<br />
+          <br />
+          <span className='highlight'>@here</span> the notification chimes,<br />
+          Breaking the flow, disrupting the rhyme.<br />
+          Lost in the chatter, the code now blurred,<br />
+          Disturbed from their focus, the rhythm deterred.<br />
+        </p>
+      </div>
     </>
   );
 }
---
+++ src/Title.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+
+
+function Title() {
+
+
+
+    return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            {/* <strike style={{ color: "red"}}> */}
+            {/* <span style={{ fontSize: '10vh' }}>
+                ❌&nbsp;&nbsp;
+            </span> */}
+            <TypeAnimation
+                sequence={[
+                    '@here',
+                    3000,
+                    '@channel',
+                    3000,
+                    '@everyone',
+                    3000
+                ]}
+                wrapper="span"
+                speed={25}
+                style={{ fontWeight: "bold", fontSize: '10vh', display: 'table', paddingBottom: "7px", paddingRight: "7px", paddingLeft: "7px", color: "#D6A939", backgroundColor: "#393329" }}
+                repeat={Infinity}
+            />
+            {/* </strike> */}
+        </div>
+    );
+}
+
+export default Title;
