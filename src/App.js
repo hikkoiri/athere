dba2c7a7af8536c1bff601f6b9eff1cb32c71e17
Carlo Hildebrandt <carlo.hildebrandt@web.de>
2024-02-16 00:27:48 +0100

    add content scaffold

--- src/App.js
+++ src/App.js
@@ -6,18 +6,83 @@ function App() {
     <>
       <div style={{ width: "90vw", maxWidth: "1296px", margin: "auto", marginTop: "5vh" }}>
         <Title />
-        <p style={{ color: "#D1D2D3", fontSize: "5vh" }}>
-          In the realm of code, a developer delves deep, <br />
-          Crafting digital worlds, where dreams do leap. <br />
-          Focused and absorbed, in the task at hand,<br />
-          Till a ping disrupts, like a shifting sand.<br />
-          <br />
-          <span className='highlight'>@here</span> the Slack notification chimes,<br />
-          Breaking the flow, disrupting the rhyme.<br />
-          Lost in the chatter, the code now blurred,<br />
-          Disturbed from their focus, the rhythm deterred.<br />
-        </p>
-      </div>
+
+        <br />
+        <br />
+        <div style={{ display: "flex" }}>
+          <div style={{ padding: "0.8vh" }}>
+            <img src='/tldr.png' alt='tldr' style={{ height: "6vh", borderRadius: "20%" }}></img>
+          </div>
+          <div style={{ paddingLeft: "10px" }}>
+            <p><b>TL;DR</b><br />
+              With great power comes great responsibility. <br />
+              Just because you can force Slack notification popups on everyone's laptop, you shouldn't abuse it for your own good. <br />
+              Please think twice about it if you really want to use <b><span className='highlight'>@here</span></b>, <b><span className='highlight'>@channel</span></b> or <b><span className='highlight'>@everyone</span></b>.
+            </p>
+          </div>
+        </div >
+
+        <br />
+        <br />
+        <div style={{ display: "flex" }}>
+          <div style={{ padding: "0.8vh" }}>
+            <img src='/tldr.png' alt='tldr' style={{ height: "6vh", borderRadius: "20%" }}></img>
+          </div>
+          <div style={{ paddingLeft: "10px" }}>
+            <p><b>Why did someone send me this link?</b><br />
+              It seems like you ... <br />
+              not relevant for the majority of notified people or no urgency to force a notification <br />
+              reflect for yourself <br />
+              Don't shoot the messenger ...
+            </p>
+          </div>
+        </div >
+
+        <br />
+        <br />
+        <div style={{ display: "flex" }}>
+          <div style={{ padding: "0.8vh" }}>
+            <img src='/tldr.png' alt='tldr' style={{ height: "6vh", borderRadius: "20%" }}></img>
+          </div>
+          <div style={{ paddingLeft: "10px" }}>
+            <p><b>But you are exaggerating right here</b><br />
+              For you it may seem like it, but ...
+              disruption, loosing focus <br />
+              sense of urgency, frustration because of ignorance<br />
+            </p>
+          </div>
+        </div >
+
+        <br />
+        <br />
+        <div style={{ display: "flex" }}>
+          <div style={{ padding: "0.8vh" }}>
+            <img src='/tldr.png' alt='tldr' style={{ height: "6vh", borderRadius: "20%" }}></img>
+          </div>
+          <div style={{ paddingLeft: "10px" }}>
+            <p><b>When to use it</b><br />
+              ✅need for instant response <br />
+              ✅emergency broadcast<br />
+              ✅urgent matter at hand<br />
+            </p>
+          </div>
+        </div >
+
+        <br />
+        <br />
+        <div style={{ display: "flex" }}>
+          <div style={{ padding: "0.8vh" }}>
+            <img src='/tldr.png' alt='tldr' style={{ height: "6vh", borderRadius: "20%" }}></img>
+          </div>
+          <div style={{ paddingLeft: "10px" }}>
+            <p><b>When not to use it</b><br />
+              ❌reminder<br />
+              ❌irrelevant stuff <br />
+              ❌things that only a small group of people are interested in<br />
+            </p>
+          </div>
+        </div >
+      </div >
     </>
   );
 }
--- src/Title.jsx
+++ src/Title.jsx
@@ -4,11 +4,14 @@ import { TypeAnimation } from 'react-type-animation';
 
 function Title() {
     return (
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
             {/* <strike style={{ color: "red"}}> */}
             {/* <span style={{ fontSize: '10vh' }}>
                 ❌&nbsp;&nbsp;
             </span> */}
+            <span style={{ fontSize: '8vh' }}>
+                Was it really necessary to use&nbsp;
+            </span>
             <TypeAnimation
                 sequence={[
                     '@here',
@@ -20,9 +23,12 @@ function Title() {
                 ]}
                 wrapper="span"
                 speed={25}
-                style={{ fontWeight: "bold", fontSize: '10vh', display: 'table', paddingBottom: "7px", paddingRight: "7px", paddingLeft: "7px", color: "#D6A939", backgroundColor: "#393329" }}
+                style={{ fontWeight: "bold", fontSize: '8vh', display: 'table', paddingBottom: "7px", paddingRight: "7px", paddingLeft: "7px", color: "#D6A939", backgroundColor: "#393329" }}
                 repeat={Infinity}
             />
+            <span style={{ fontSize: '8vh' }}>
+                &nbsp;?
+            </span>
             {/* </strike> */}
         </div>
     );
